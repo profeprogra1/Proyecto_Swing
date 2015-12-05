c6dc9d89d50d3f009665cc6eddd8c85211542c44
ricardo <ricardo@mbp-de-ricardo>
2015-12-05 10:12:49 -0300

    Swing 00, 10:12

---
+++ src/swing00/Swing00.java
@@ -0,0 +1,9 @@
+package swing00;
+public class Swing00 {
+    public static void main(String[] args) {
+        
+    
+    }
+}
+
+
---
+++ src/swing00/VenanaPrincipal.java
@@ -0,0 +1,10 @@
+package swing00;
+//LIBRERIA SWING
+import javax.swing.*;
+public class VenanaPrincipal extends JFrame {
+    public VenanaPrincipal(){
+        this.setSize(500,500);
+        this.setTitle("Mi primera Ventana");
+        this.setVisible(true);
+    }
+}
