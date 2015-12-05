e697c83155c4a6d9f4decb3b8877b806ce6c2469
ricardo <ricardo@10.34.12.254>
2015-12-05 11:41:07 -0300

    Swing 00, 11:40

--- src/swing00/VenanaPrincipal.java
+++ src/swing00/VenanaPrincipal.java
@@ -1,6 +1,7 @@
 package swing00;
 //LIBRERIA SWING
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import javax.swing.*;
 public class VenanaPrincipal extends JFrame {
     PanelPrincipal p=new PanelPrincipal();
@@ -15,37 +16,20 @@ public class VenanaPrincipal extends JFrame {
 }
 
 class PanelPrincipal extends JPanel{
-    JButton btn1=new JButton("MI boton");
-    JButton btn2=new JButton("MI Segundo Btn");
-    JButton btn3=new JButton("tercer");
-    JButton btn4=new JButton("cuarto");
-    JButton btn5=new JButton("quinto");
-    JTextField txt1=new JTextField();
-    //MENU
-    JMenuBar barra=new JMenuBar();
-    JMenu mnuArchivo=new JMenu("Archivo");
-    JMenuItem mnuSalir=new JMenuItem("Salir");
-    JMenu mnuEdit=new JMenu("Edit");
-    JMenuItem mnuUndo=new JMenuItem("Undo");
-    
-    JTextArea txtArea=new JTextArea();
-    JScrollPane scroll=new JScrollPane(txtArea);
-    
-     public PanelPrincipal(){
-        barra.add(mnuArchivo);
-        mnuArchivo.add(mnuSalir);
-        mnuSalir.setEnabled(false);
-        mnuArchivo.addSeparator();
-        
-        barra.add(mnuEdit);
-        mnuEdit.add(mnuUndo);
-        this.setLayout(new BorderLayout());
-        this.add(barra,BorderLayout.NORTH);
-        this.add(scroll,BorderLayout.CENTER);
-        //this.add(btn3,BorderLayout.SOUTH);
-        //this.add(txt1,BorderLayout.SOUTH);
-        //this.add(btn4,BorderLayout.EAST);
-        //this.add(btn5,BorderLayout.WEST);
+    JLabel lblNombre=new JLabel("Nombre:");
+    JTextField txtNombre=new JTextField();
+    JLabel lblApellido=new JLabel("Apellido");
+    JTextField txtApellido=new JTextField();
+    JButton btnMostrar=new JButton("Mostrar");
+    JButton btnLimpiar=new JButton("Limpiar");
+    public PanelPrincipal(){
+        this.setLayout(new GridLayout(3,2));
+        this.add(lblNombre);
+        this.add(txtNombre);
+        this.add(lblApellido);
+        this.add(txtApellido);
+        this.add(btnMostrar);
+        this.add(btnLimpiar);
         
     }
 }
