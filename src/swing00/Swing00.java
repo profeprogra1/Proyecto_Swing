05af8bfb0a0ed2736624ee12edce3dd6bcf6d62a
ricardo <ricardo@10.34.12.254>
2015-12-10 21:18:58 -0300

    Domingo 10 Dic 2015, 21:20

--- src/swing00/Swing00.java
+++ src/swing00/Swing00.java
@@ -1,8 +1,8 @@
 package swing00;
 public class Swing00 {
     public static void main(String[] args) {
+        Ventana v=new Ventana();
         
-    
     }
 }
 
---
+++ src/swing00/Ventana.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swing00;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ricardo
+ */
+public class Ventana extends JFrame {
+    
+    public Ventana(){
+        this.setTitle("CRUD JAVA SWING");
+        this.setSize(500, 500);
+        this.setVisible(true);
+    }
+}
