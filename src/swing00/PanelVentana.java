673cd245a0abe6f27b6a60273877aa51d420fdcd
ricardo <ricardo@10.34.12.254>
2015-12-10 22:01:33 -0300

    Domingo 10 Dic 2015, 22:00

--- src/swing00/PanelVentana.java
+++ src/swing00/PanelVentana.java
@@ -5,6 +5,7 @@
  */
 package swing00;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -18,12 +19,29 @@ public class PanelVentana extends JPanel {
     JTextField txtNombre=new JTextField();
     JLabel lblApellido=new JLabel("Apellido");
     JTextField txtApellido=new JTextField();
+    JLabel lblCiudad=new JLabel("Ciudad");
+    JTextField txtCiudad=new JTextField();
+    JButton btnAgregar=new JButton("Agregar");
+    JButton btnLimpiar=new JButton("Limpiar");
     public PanelVentana(){
         this.setLayout(null);
         lblNombre.setBounds(20,20,120,20);
         this.add(lblNombre);
         txtNombre.setBounds(150,20,120,20);
         this.add(txtNombre);
+        lblApellido.setBounds(20,50,120, 20);
+        this.add(lblApellido);
+        txtApellido.setBounds(150,50,120,20);
+        this.add(txtApellido);
+        lblCiudad.setBounds(20,80,120,20);
+        txtCiudad.setBounds(150,80,120,20);
+        this.add(txtCiudad);
+        btnAgregar.setBounds(20,110,120,20);
+        this.add(btnAgregar);
+        btnLimpiar.setBounds(150,110,120,20);
+        this.add(btnLimpiar);
+        
+       
         
         
     }
--- src/swing00/Ventana.java
+++ src/swing00/Ventana.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package swing00;
-
 import javax.swing.JFrame;
-
-/**
- *
- * @author ricardo
- */
 public class Ventana extends JFrame {
     PanelVentana p=new PanelVentana();
     public Ventana(){
